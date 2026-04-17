2f408ac34ec487a5115984e87271fcb0b44aca8d
John Mike Uy <uyjohnmike2341@gmail.com>
2026-04-18 00:44:55 +0800

    Added new project folders to greenhouse

--- ngrok_camera.ts
+++ ngrok_camera.ts
@@ -3,6 +3,6 @@
 export const CAMERA_URL: string = "https://inexpensive-acids-consultancy-alliance.trycloudflare.com/video_feed1";
 export const CAMERA_URL2: string = "https://inexpensive-acids-consultancy-alliance.trycloudflare.com/video_feed2";
 
-export const NGROK_URL: string = "https://lorita-orthopnoeic-domitila.ngrok-free.dev";
+export const NGROK_URL: string = "https://piquillogreenhouse.onrender.com";
 
 export const SUPABASE_IMAGE_PATH: string = "https://xvebncyvecfvocnqcxpk.supabase.co/storage/v1/object/public/images/";
